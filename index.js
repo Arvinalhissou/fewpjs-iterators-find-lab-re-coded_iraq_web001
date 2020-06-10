bb24f27646f28cf1032ff21223d46615ed564144
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-10 08:14:56 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+];
+
+superbowlWin = (record) => {
+  let res = record.find( record => record.result === "W" )
+  return !!res ? res.year : undefined
+}
