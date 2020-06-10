5b789af9a0c28786ccdac6a9d49051b2eaaa4d73
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-10 08:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+];
+
+const find = record.find(item => item.result === 'W'){
+return item.yea}
+})
